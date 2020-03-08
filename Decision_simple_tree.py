c2fbfaa3ff3f08133763fb962d70eb67b3c2618b
Pamela M <61203913+grigri2020@users.noreply.github.com>
2020-03-08 16:04:37 -0400

    Update Decision_simple_tree.py

--- Decision_simple_tree.py
+++ Decision_simple_tree.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tues Aug  6 15:32:37 2019
+Created on Tue Aug  6 15:32:37 2019
 
 @author: Pamela Mishra
+The one who will climb all 7 summits and more! 
 """
 
 
@@ -18,32 +19,61 @@ from sklearn.externals.six import StringIO
 from pydot import graph_from_dot_data
 from IPython.display import Image
 
+#rEFER DECISION TREE TUTORIAL:
+#https://www.hackerearth.com/practice/machine-learning/machine-learning-algorithms/ml-decision-tree/tutorial/
 
 
+class Decision_tree:
+    def __init__(self, file_df):
+        self.file_df = file_df
+        
+    #read file into a dataframe
+    def read_file(self, file_name):
+        x  = pd.read_csv(file_name, sep=",")
+        return x
 
-os.chdir("/Users/mishrap1/KAGGLE/Decision_tree_kaggle")
+    #Make feature set for the decision tree, Excluding 1 column
+    #All the features used to predict whether the patient will get cancer 
+    #or not
+    def make_features(self, data_frame, column_not_included):
+        features = data_frame.loc[:, data_frame.columns != column_not_included]
+        return features
+    
+    #Make the target to be predicted
+    #iN THIS CASE: ITS WHETHER THE PATIENT HAS CANCER OR NOT
+    def make_target(self, data_frame, column_included):
+        target   = data_frame[column_included].astype('bool')
+        return target
 
-x  = pd.read_csv("input/kag_risk_factors_cervical_cancer.csv", sep=",")
-features = x.loc[:, x.columns != 'Dx:Cancer']
-features= x[['Age', 'STDs', 'IUD']]
-target   = x['Dx:Cancer'].astype('bool')
-x=x.replace('?','0')
+    #Split the data into traning and testing
+    #Use random seed =1 so that this can be replicated
+    def split_data_frame(self, features, target):
+        features_train, features_test, target_train, target_test =\
+        train_test_split(features, target, random_state=1)
+        return(features_train, features_test, target_train, target_test)
 
+def main():
+    input = "input/kag_risk_factors_cervical_cancer.csv"
+    my_dec = Decision_tree(input)
+    df = my_dec.read_file(input)
+    df=df.replace('?','0')
+    df_features = my_dec.make_features(df,"Dx:Cancer")
+    df_target  =  my_dec.make_target(df, "Dx:Cancer")
+    (features_train, features_test, target_train, target_test ) = my_dec.split_data_frame(df_features, df_target)
+    
+    
+    #Make a decision tree cxlassifier object
+    dec_tree = DecisionTreeClassifier()
+    feature_names =  features_train.columns
+    dec_tree.fit(features_train,target_train)
+    dot_data = StringIO()
+    export_graphviz(dec_tree, out_file=dot_data,feature_names=feature_names)
+    (graph, ) = graph_from_dot_data(dot_data.getvalue())
+    Image(graph.create_png())
+    graph.write_png("Decision_Tree_cervical.png")
+    
+    
+#This is where it all began
+if __name__ == "__main__": main()
 
-
-#Drop the NA values if exists
-features = x.loc[:, x.columns != 'Dx:Cancer']
-target   = x['Dx:Cancer'].astype('bool')
-features_train, features_test, target_train, target_test = train_test_split(features, target, random_state=1)
-dec_tree = DecisionTreeClassifier()
-
-feature_names =  features_train.columns
-dec_tree.fit(features_train,target_train)
-dot_data = StringIO()
-export_graphviz(dec_tree, out_file=dot_data,feature_names=feature_names)
-(graph, ) = graph_from_dot_data(dot_data.getvalue())
-Image(graph.create_png())
-#y_pred = dec_tree.predict(features_test)
-#print(y_pred)
-graph.write_png("Decision_Tree_cervical.png")
 
